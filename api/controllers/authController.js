dc42a510587049b1a4505cef91cfc97104412157
Leki Tshering <lekitshering@academy.bt>
2024-02-28 16:40:52 +0600

    sign in page

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -27,5 +27,7 @@ export const signin = async (req, res, next) => {
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .json(rest);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
