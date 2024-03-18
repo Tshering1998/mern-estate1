4172bae2b468311968766158c6257768523acfac
Leki Tshering <lekitshering@academy.bt>
2024-03-18 17:00:19 +0600

    search Funtionality UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import { Toaster } from "react-hot-toast";
 import Private from "./components/Private";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 export default function App() {
   return (
@@ -21,6 +22,8 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
+
         <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<Private />}>
           <Route path="/profile" element={<Profile />} />
