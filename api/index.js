ece0d228da454d99eba37947e2fb9909d838c51c
Leki Tshering <lekitshering@academy.bt>
2024-03-19 23:17:24 +0600

    new

--- api/index.js
+++ api/index.js
@@ -28,14 +28,13 @@ app.listen(3000, () => {
   console.log("Server is running on 3000");
 });
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 //routes
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
-
-app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
